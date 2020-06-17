5a79efe2b915d95ebbaa7cef3e7a219b7b8b28da
Geezar <nirozaz@yahoo.com>
2020-06-17 14:52:08 +0300

    Updates

---
+++ precache-manifest.cf8d9b977a2f2720400409cbe240afa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e21a3467fa15d6cdb717b52e2aba98a",
+    "url": "/roborolodex/index.html"
+  },
+  {
+    "revision": "e78e493bad097e08bb4e",
+    "url": "/roborolodex/static/css/main.26148d86.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/roborolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roborolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78e493bad097e08bb4e",
+    "url": "/roborolodex/static/js/main.f8d0af0f.chunk.js"
+  },
+  {
+    "revision": "b1ba4e91668b340c9278",
+    "url": "/roborolodex/static/js/runtime-main.4784151d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roborolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
